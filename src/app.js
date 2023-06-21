831f852865222b78ee8d7dcdeb4a934ee70b81b4
IsabelGouveia <131471952+IsabelGouveia@users.noreply.github.com>
2023-06-21 18:59:25 +0000

    coding guideline

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generatorExcuse();
 };
+
+const generatorExcuse = () => {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const excuse = [
+    getRandomElement(who) +
+      " " +
+      getRandomElement(action) +
+      " " +
+      getRandomElement(what) +
+      " " +
+      getRandomElement(when)
+  ];
+
+  return excuse;
+};
+
+function getRandomElement(yourArray) {
+  let randomIndex = Math.floor(Math.random() * yourArray.length);
+  return yourArray[randomIndex];
+}
